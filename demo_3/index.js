93a12ec86c15d2c04ecdb68c5f12012b312fb993
moajo <mimirosiasd@gmail.com>
2016-12-27 13:03:22 +0900

    fix demo

--- demo_3/index.js
+++ demo_3/index.js
@@ -1,8 +1,8 @@
 $(function () {
-  var i = 0;
-  $("input").on("change", function () {
-    i++;
-    var tag = `<mesh geometry="cube" position="${i * 2.2},0,0" color="${this.value}"/>`
-    gr("#main")("mesh").first().append(tag);
-  });
-});
+    var count = 0;
+    $("input").on("change", function () {
+        count++;
+        var tag = `<mesh geometry="cube" position="${count * 2.2},0,0" color="${this.value}"/>`;
+        gr("#main")("mesh").first().append(tag);
+    });
+})
